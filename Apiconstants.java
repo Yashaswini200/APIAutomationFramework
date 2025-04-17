e9eae9b6cd14e0ef012693d6efc7955df5752b0d
Yashaswini <110585291+Yashaswini200@users.noreply.github.com>
2025-04-17 21:17:41 +0530

    Add files via upload

---
+++ Apiconstants.java
@@ -0,0 +1,8 @@
+package org.example.EndPoints;
+
+public class Apiconstants {
+    public static String Base_url="";
+    public static String create_update_booking_url="/booking";
+    public static String Auth_url="/auth";
+    public static String Ping_url="/ping";
+}
