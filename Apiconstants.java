e5a913507e252818a71ce51a187909fe94bb17c9
Yashaswini <110585291+Yashaswini200@users.noreply.github.com>
2025-04-17 21:19:32 +0530

    Delete Apiconstants.java

--- Apiconstants.java
+++
@@ -1,8 +0,0 @@
-package org.example.EndPoints;
-
-public class Apiconstants {
-    public static String Base_url="";
-    public static String create_update_booking_url="/booking";
-    public static String Auth_url="/auth";
-    public static String Ping_url="/ping";
-}
